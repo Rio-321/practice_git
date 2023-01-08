af7073425fdc386803fd84930bb99927afd984fb
Rio -321 <iit2019024@iiita.ac.in>
2023-01-08 23:26:49 +0530

    1st commit

---
+++ rough.js
@@ -0,0 +1,10 @@
+ var x = 5 + 6;
+ var y = x * 10;
+ var z = 7;
+ var sop;
+ sop = (x*y)+z;
+
+function MyFunction(){
+    document.getElementById("demo").innerHTML = "SOP of three numbers is " + y;
+   
+}
